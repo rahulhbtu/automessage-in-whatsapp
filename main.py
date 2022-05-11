519463c5aef47710fdd81f22722c699c31252d08
Rahul Verma <95970334+rahulhbtu@users.noreply.github.com>
2022-05-11 09:14:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 1234567890', 'happy birthday to u deepak ', 9, 9)
+# where 1234567890 is mobile number
+# and happy birthday   is message that u  want to  type and 9,9 is the hour and minute 
+
+
+
